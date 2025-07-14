ca00401a939ad15c07b1ae943af3f8ba971581ed
Ansh Saxena <anshsaxena4190@gmail.com>
2025-07-14 12:21:14 +0530

    Update links.ts

--- config/links.ts
+++ config/links.ts
@@ -1,3 +1,3 @@
 export const CONSOLE_LINK =
   process.env.NEXT_PUBLIC_CONSOLE_URL || "https://console.aquanode.io/";
-export const CONTACT_SALES_LINK = "https://calendly.com/anshss/call";
+export const CONTACT_SALES_LINK = "https://cal.com/anshs3/15min";

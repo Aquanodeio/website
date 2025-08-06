135cb74a3f88b8faa4dca19b426c70dd8de58811
Cygnus <cygnusdev04@gmail.com>
2025-08-06 10:55:50 +0530

    updates metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,37 @@
 import { Inter, Figtree } from "next/font/google";
+import type { Metadata } from "next";
+
 import "./globals.css";
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
-const figtree = Figtree({ 
-  subsets: ["latin"], 
+const figtree = Figtree({
+  subsets: ["latin"],
   variable: "--font-figtree",
-  weight: ["300", "400", "500", "600", "700"]
+  weight: ["300", "400", "500", "600", "700"],
 });
+
+export const metadata: Metadata = {
+  title: "Aquanode",
+  description: "AI Cloud that's Effortless & Affordable",
+  openGraph: {
+    title: "Aquanode - AI Cloud that's Effortless & Affordable",
+    description: "AI Cloud that's Effortless & Affordable",
+    url: "https://aquanode.io",
+    siteName: "Aquanode",
+    images: [
+      {
+        url: "https://aquanode.io/images/aquanode-banner.png",
+        width: 1200,
+        height: 630,
+        alt: "Aquanode Platform",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    images: ["https://aquanode.io/images/aquanode-banner.png"],
+  },
+};
 
 export default function RootLayout({
   children,

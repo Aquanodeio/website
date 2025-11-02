dfd2936d84db863779abf5391e62c7a4415cae52
Ansh Saxena <anshsaxena4190@gmail.com>
2025-11-02 18:25:54 +0530

    merge conflict

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import HeroSection from "@/components/Home/HeroSection";
 import FeaturesSection from "@/components/Home/FeaturesSection";
 import GPUPickerSection from "@/components/Home/GPUPickerSection";
 import TestimonialsSection from "@/components/Home/TestimonialsSection";
+import WhatYouCanDoSection from "@/components/Home/WhatYouCanDoSection";
 import PricingComparisonSection from "@/components/Home/PricingComparisonSection";
 import WhyChooseSection from "@/components/Home/WhyChooseSection";
 import FooterCTA from "@/components/Home/FooterCTA";
@@ -45,6 +46,9 @@ export default function Home() {
 
       {/* Testimonials Section */}
       <TestimonialsSection />
+
+      {/* What You Can Do Section */}
+      <WhatYouCanDoSection />
 
       {/* Pricing Comparison Section */}
       <PricingComparisonSection />
